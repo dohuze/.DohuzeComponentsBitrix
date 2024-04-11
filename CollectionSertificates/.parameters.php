674994d664c94f82364a0667ed43d07ec23eb49c
dohuze <sgusarova@mail.ru>
2024-04-11 16:14:28 +0500

    Add files via upload

---
+++ CollectionSertificates/.parameters.php
@@ -0,0 +1,13 @@
+<?	if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+	$arComponentParameters = array(
+		"GROUPS" => array(),
+		"PARAMETERS" => array(
+			"ID_IBLOCK_SERNIFICATES" => array(
+				"PARENT" => "BASE",
+				"NAME" => "ID инфоблока сертификатов",
+				"TYPE" => "STRING",
+				"MULTIPLE" => "N",
+				"DEFAULT" => "1",
+			),
+		),
+	);
